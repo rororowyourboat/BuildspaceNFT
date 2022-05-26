f2d705bdc1e83fe15ee5d98f0d2e4fb41029d2a1
Rohan Mehta <e4rohan@gmail.com>
2022-05-26 05:10:53 +0000

    changed to sciecntist

--- contracts/ScientistPartner.sol
+++ contracts/ScientistPartner.sol
@@ -14,12 +14,12 @@ contract MyEpicNFT is ERC721URIStorage {
 
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
-  string[] firstWords = ["Revenge", "Love", "Disgust", "Hate", "Jealousy", "Hobby"];
-  string[] secondWords = ["Park", "Office", "Bedroom", "Woods", "Concert", "Bar", "Road"];
-  string[] thirdWords = ["Axe", "Sniper", "Poison", "Scythe", "Fist", "Hammer", "Crossbow", "Shotgun", "Rope", "Shuriken"];
+  string[] firstWords = ["Einstein", "Gauss", "Euler", "Lagrange", "Newton", "Bernoulli", "Aryabhatta", "Turing","Bohr","Heisenberg","Noether","Curie"];
+  string[] secondWords = ["Math","Hockey","Basketball","Chess","Singing","Dance","Football","TableTennis", "Jumprope"];
+  // string[] thirdWords = [""];
 
 
-  constructor() ERC721 ("PsychopathNFT", "MORT") {
+  constructor() ERC721 ("ScientistPartnerNFT", "SCIE") {
     console.log("This is my NFT contract. Woah!");
   }
 
@@ -35,11 +35,11 @@ contract MyEpicNFT is ERC721URIStorage {
     return secondWords[rand];
   }
 
-  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
-    rand = rand % thirdWords.length;
-    return thirdWords[rand];
-  }
+  // function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+  //   uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+  //   rand = rand % thirdWords.length;
+  //   return thirdWords[rand];
+  // }
 
   function random(string memory input) internal pure returns (uint256) {
       return uint256(keccak256(abi.encodePacked(input)));
@@ -50,8 +50,8 @@ contract MyEpicNFT is ERC721URIStorage {
 
     string memory first = pickRandomFirstWord(newItemId);
     string memory second = pickRandomSecondWord(newItemId);
-    string memory third = pickRandomThirdWord(newItemId);
-    string memory combinedWord = string(abi.encodePacked(first, second, third));
+    // string memory third = pickRandomThirdWord(newItemId);
+    string memory combinedWord = string(abi.encodePacked(first, second));
 
     string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
 
